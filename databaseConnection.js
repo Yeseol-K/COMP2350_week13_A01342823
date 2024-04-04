2c923722850a1d2a1131f24afcd5e7a82e77683b
unknown <ye37seol@gmailcom>
2024-04-04 14:37:12 -0700

    fix connnection

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,7 +1,7 @@
 const database = require("mongoose");
 const is_qoddi = process.env.IS_QODDI || false;
 const databaseName = "lab_example";
-const qoddiURI = "mongodb+srv://mongodb+srv://johnsmith890328:gftScSTsQzFLuw5L@cluster0.zjxitka.mongodb.net/" + databaseName + "?retryWrites=true&w=majority";
+const qoddiURI = "mongodb+srv://johnsmith890328:gftScSTsQzFLuw5L@cluster0.zjxitka.mongodb.net/" + databaseName + "?retryWrites=true&w=majority";
 
 const localURI = "mongodb://localhost/" + databaseName + "?authSource=admin&retryWrites=true";
 if (is_qoddi) {
